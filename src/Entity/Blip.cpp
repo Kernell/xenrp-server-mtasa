9e0e048ea6d014db12abdafde7a524967cce9463
Kernell <kernell.it@gmail.com>
2017-03-04 01:16:05 +0300

    Добавлены Lua методы для Blip

--- src/Entity/Blip.cpp
+++ src/Entity/Blip.cpp
@@ -13,11 +13,95 @@
 #include "StdInc.h"
 #include "Blip.h"
 
+#include "Core/Module.h"
+
 Blip::Blip( PVOID luaUserdata ) :
 	Element( luaUserdata )
 {
 }
 
+Blip::Blip( const Vector3& position, BlipIcon icon, uchar size, Color color, short ordering, ushort visibleDistance, Element* visibleTo ) :
+	Element( nullptr )
+{
+	this->LuaUserdata = Lua::Bindings::CreateBlip( LuaVM, position, icon, size, color, ordering, visibleDistance, visibleTo ? visibleTo->GetLuaUserdata() : nullptr );
+}
+
+Blip::Blip( Element* target, BlipIcon icon, uchar size, Color color, short ordering, ushort visibleDistance, Element* visibleTo ) :
+	Element( nullptr )
+{
+	this->LuaUserdata = Lua::Bindings::CreateBlipAttachedTo( LuaVM, target->GetLuaUserdata(), icon, size, color, ordering, visibleDistance, visibleTo ? visibleTo->GetLuaUserdata() : nullptr );
+}
+
 Blip::~Blip()
 {
+}
+
+BlipIcon Blip::GetIcon() const
+{
+	uchar value;
+
+	Lua::Bindings::GetBlipIcon( LuaVM, LuaUserdata, value );
+
+	return (BlipIcon)value;
+}
+
+uchar Blip::GetSize() const
+{
+	uchar value;
+
+	Lua::Bindings::GetBlipSize( LuaVM, LuaUserdata, value );
+
+	return value;
+}
+
+Color Blip::GetColor() const
+{
+	Color value;
+
+	Lua::Bindings::GetBlipColor( LuaVM, LuaUserdata, value );
+
+	return value;
+}
+
+short Blip::GetOrdering() const
+{
+	short value;
+
+	Lua::Bindings::GetBlipOrdering( LuaVM, LuaUserdata, value );
+
+	return value;
+}
+
+ushort Blip::GetVisibleDistance() const
+{
+	ushort value;
+
+	Lua::Bindings::GetBlipVisibleDistance( LuaVM, LuaUserdata, value );
+
+	return value;
+}
+
+bool Blip::SetIcon( BlipIcon icon )
+{
+	return Lua::Bindings::SetBlipIcon( LuaVM, LuaUserdata, (uchar)icon );
+}
+
+bool Blip::SetSize( uchar size )
+{
+	return Lua::Bindings::SetBlipSize( LuaVM, LuaUserdata, size );
+}
+
+bool Blip::SetColor( const Color& color )
+{
+	return Lua::Bindings::SetBlipColor( LuaVM, LuaUserdata, color );
+}
+
+bool Blip::SetOrdering( short ordering )
+{
+	return Lua::Bindings::SetBlipOrdering( LuaVM, LuaUserdata, ordering );
+}
+
+bool Blip::SetVisibleDistance( ushort visibleDistance )
+{
+	return Lua::Bindings::SetBlipVisibleDistance( LuaVM, LuaUserdata, visibleDistance );
 }
--- src/Entity/Blip.h
+++ src/Entity/Blip.h
@@ -17,11 +17,95 @@ class Blip;
 
 #include "Element.h"
 
+enum BlipIcon
+{
+	None = 0,
+	Border,
+	Centre,
+	MapHere,
+	North,
+
+	AirYard,
+	Gun,
+	Barbers,
+	BigSmoke,
+	BoatYard,
+	Burgershot,
+	Bulldozer,
+	CatPink,
+	Cesar,
+	Chicken,
+	CJ,
+	Crash1,
+	Diner,
+	EmmetGun,
+	EnemyAttack,
+	Fire,
+	Girlfriend,
+	Hospital,
+	Coco,
+	Maddog,
+	Mafia,
+	Mcstrap,
+	ModGarage,
+	Ogloc,
+	Pizza,
+	Police,
+	PropertyGreen,
+	PropertyRed,
+	Race,
+	Ryder,
+	SaveHouse,
+	School,
+	Mystery,
+	Sweet,
+	Tattoo,
+	Truth,
+	Waypoint,
+	TorenoRanch,
+	Triads,
+	TriadsCasino,
+	Tshirt,
+	Woozie,
+	Zero,
+	DateDisco,
+	DateDrink,
+	DateFood,
+	Truck,
+	Cash,
+	Flag,
+	Gym,
+	Impound,
+	RunwayLight,
+	Runway,
+	GangB,
+	GangP,
+	GangY,
+	GangN,
+	GangG,
+	Spray
+};
+
 class Blip : public Element
 {
 public:
-												Blip                     ( PVOID luaUserdata );
-	virtual                                     ~Blip                    ();
+									Blip                     ( PVOID luaUserdata );
+	                                Blip                     ( const Vector3& position, BlipIcon icon = BlipIcon::None, uchar size = 2, Color color = Color(), short ordering = 0, ushort visibleDistance = 9999, Element* visibleTo = nullptr );
+	                                Blip                     ( Element* target, BlipIcon icon = BlipIcon::None, uchar size = 2, Color color = Color(), short ordering = 0, ushort visibleDistance = 9999, Element* visibleTo = nullptr );
+	virtual                         ~Blip                    ();
+
+    
+	virtual BlipIcon                GetIcon                  () const;
+	virtual uchar                   GetSize                  () const;
+	virtual Color                   GetColor                 () const;
+	virtual short                   GetOrdering              () const;
+	virtual ushort                  GetVisibleDistance       () const;
+
+	virtual bool                    SetIcon                  ( BlipIcon icon );
+	virtual bool                    SetSize                  ( uchar size );
+	virtual bool                    SetColor                 ( const Color& color );
+	virtual bool                    SetOrdering              ( short ordering );
+	virtual bool                    SetVisibleDistance       ( ushort visibleDistance );
 };
 
 #endif
