9df65f051e2e03168c41f8cfb2ccbd40fa87281c
Kernell <kernell.it@gmail.com>
2017-03-04 01:39:44 +0300

    Добавлены Lua методы для RadarArea

--- src/Entity/RadarArea.cpp
+++ src/Entity/RadarArea.cpp
@@ -13,11 +13,66 @@
 #include "StdInc.h"
 #include "RadarArea.h"
 
+#include "Core/Module.h"
+
 RadarArea::RadarArea( PVOID luaUserdata ) :
 	Element( luaUserdata )
 {
 }
 
+RadarArea::RadarArea( const Vector2& position, const Vector2& size, const Color& color, Element* visibleTo ) :
+	Element( nullptr )
+{
+	this->LuaUserdata = Lua::Bindings::CreateRadarArea( LuaVM, position, size, color, visibleTo ? visibleTo->GetLuaUserdata() : nullptr );
+}
+
 RadarArea::~RadarArea()
 {
+}
+
+Vector2 RadarArea::GetSize() const
+{
+	Vector2 value;
+
+	Lua::Bindings::GetRadarAreaSize( LuaVM, LuaUserdata, value );
+
+	return value;
+}
+
+Color RadarArea::GetColor() const
+{
+	Color value;
+
+	Lua::Bindings::GetRadarAreaColor( LuaVM, LuaUserdata, value );
+
+	return value;
+}
+
+bool RadarArea::IsFlashing() const
+{
+	return Lua::Bindings::IsRadarAreaFlashing( LuaVM, LuaUserdata );
+}
+
+bool RadarArea::IsInside( const Vector2& position ) const
+{
+	bool value;
+
+	Lua::Bindings::IsInsideRadarArea( LuaVM, LuaUserdata, position, value );
+
+	return value;
+}
+
+bool RadarArea::SetSize( const Vector2& vecSize )
+{
+	return Lua::Bindings::SetRadarAreaSize( LuaVM, LuaUserdata, vecSize );
+}
+
+bool RadarArea::SetColor( const Color& color )
+{
+	return Lua::Bindings::SetRadarAreaColor( LuaVM, LuaUserdata, color );
+}
+
+bool RadarArea::SetFlashing( bool flashing )
+{
+	return Lua::Bindings::SetRadarAreaFlashing( LuaVM, LuaUserdata, flashing );
 }
--- src/Entity/RadarArea.h
+++ src/Entity/RadarArea.h
@@ -20,8 +20,19 @@ class RadarArea;
 class RadarArea : public Element
 {
 public:
-												RadarArea                     ( PVOID luaUserdata );
-	virtual                                     ~RadarArea                    ();
+										RadarArea      ( PVOID luaUserdata );
+										RadarArea      ( const Vector2& position, const Vector2& size, const Color& color, Element* visibleTo = nullptr );
+	virtual                             ~RadarArea     ();
+
+
+	virtual Vector2                     GetSize        () const;
+	virtual Color                       GetColor       () const;
+	virtual bool                        IsFlashing     () const;
+	virtual bool                        IsInside       ( const Vector2& position ) const;
+
+	virtual bool                        SetSize        ( const Vector2& vecSize );
+	virtual bool                        SetColor       ( const Color& color );
+	virtual bool                        SetFlashing    ( bool flashing );
 };
 
 #endif
