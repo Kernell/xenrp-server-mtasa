724c145199564de77d93c76179caae224f2c56e9
Kernell <kernell.it@gmail.com>
2017-03-04 00:42:15 +0300

    Добавлены Lua методы для Marker

--- src/Entity/Marker.cpp
+++ src/Entity/Marker.cpp
@@ -13,11 +13,103 @@
 #include "StdInc.h"
 #include "Marker.h"
 
+#include "Core/Module.h"
+
+const string Marker::MARKER_TYPE_CHECKPOINT = "checkpoint";
+const string Marker::MARKER_TYPE_RING       = "ring";
+const string Marker::MARKER_TYPE_CYLINDER   = "cylinder";
+const string Marker::MARKER_TYPE_ARROW      = "arrow";
+const string Marker::MARKER_TYPE_CORONA     = "corona";
+
 Marker::Marker( PVOID luaUserdata ) :
 	Element( luaUserdata )
 {
 }
 
+Marker::Marker( const Vector3& position, const string& type, float size, const Color& color, Element* visibleTo ) :
+	Element( nullptr )
+{
+	this->LuaUserdata = Lua::Bindings::CreateMarker( LuaVM, position, type.c_str(), size, color, visibleTo ? visibleTo->GetLuaUserdata() : nullptr );
+}
+
 Marker::~Marker()
 {
+}
+
+string Marker::GetType() const
+{
+	char* value = new char[ 32 ];
+
+	Lua::Bindings::GetMarkerType( LuaVM, LuaUserdata, value );
+
+	string str( value );
+
+	delete [] value;
+
+	return str;
+}
+
+float Marker::GetSize() const
+{
+	float value;
+
+	Lua::Bindings::GetMarkerSize( LuaVM, LuaUserdata, value );
+
+	return value;
+}
+
+Color Marker::GetColor() const
+{
+	Color value;
+
+	Lua::Bindings::GetMarkerColor( LuaVM, LuaUserdata, value );
+
+	return value;
+}
+
+Vector3 Marker::GetTarget() const
+{
+	Vector3 value;
+
+	Lua::Bindings::GetMarkerTarget( LuaVM, LuaUserdata, value );
+
+	return value;
+}
+
+string Marker::GetIcon() const
+{
+	char* value = new char[ 32 ];
+
+	Lua::Bindings::GetMarkerIcon( LuaVM, LuaUserdata, value );
+
+	string str( value );
+
+	delete[] value;
+
+	return str;
+}
+
+bool Marker::SetType( const string& type )
+{
+	return Lua::Bindings::SetMarkerType( LuaVM, LuaUserdata, type.c_str() );
+}
+
+bool Marker::SetSize( float size )
+{
+	return Lua::Bindings::SetMarkerSize( LuaVM, LuaUserdata, size );
+}
+
+bool Marker::SetColor( const Color& color )
+{
+	return Lua::Bindings::SetMarkerColor( LuaVM, LuaUserdata, color );
+}
+
+bool Marker::SetTarget( const Vector3& target )
+{
+	return Lua::Bindings::SetMarkerTarget( LuaVM, LuaUserdata, target );
+}
+
+bool Marker::SetIcon( const string& icon )
+{
+	return Lua::Bindings::SetMarkerIcon( LuaVM, LuaUserdata, icon.c_str() );
 }
--- src/Entity/Marker.h
+++ src/Entity/Marker.h
@@ -20,8 +20,29 @@ class Marker;
 class Marker : public Element
 {
 public:
-												Marker                     ( PVOID luaUserdata );
-	virtual                                     ~Marker                    ();
+									Marker                    ( PVOID luaUserdata );
+									Marker                    ( const Vector3& position, const string& type, float size, const Color& color, Element* visibleTo = nullptr );
+	virtual                         ~Marker                   ();
+
+///	virtual uint                    GetCount                  () const;
+	virtual string                  GetType                   () const;
+	virtual float                   GetSize                   () const;
+	virtual Color                   GetColor                  () const;
+	virtual Vector3                 GetTarget                 () const;
+	virtual string                  GetIcon                   () const;
+
+	virtual bool                    SetType                   ( const string& type );
+	virtual bool                    SetSize                   ( float size );
+	virtual bool                    SetColor                  ( const Color& color );
+	virtual bool                    SetTarget                 ( const Vector3& target );
+	virtual bool                    SetIcon                   ( const string& icon );
+
+public:
+	static const string MARKER_TYPE_CHECKPOINT;
+	static const string MARKER_TYPE_RING;
+	static const string MARKER_TYPE_CYLINDER;
+	static const string MARKER_TYPE_ARROW;
+	static const string MARKER_TYPE_CORONA;
 };
 
 #endif
