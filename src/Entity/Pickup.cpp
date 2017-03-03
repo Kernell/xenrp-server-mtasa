0a9ac4a68ff43f8424797a707d99bcd1ab5af661
Kernell <kernell.it@gmail.com>
2017-03-04 02:00:29 +0300

    Добавлены Lua методы для Pickup

--- src/Entity/Pickup.cpp
+++ src/Entity/Pickup.cpp
@@ -13,11 +13,88 @@
 #include "StdInc.h"
 #include "Pickup.h"
 
+#include "Core/Module.h"
+
 Pickup::Pickup( PVOID luaUserdata ) :
 	Element( luaUserdata )
 {
 }
 
+Pickup::Pickup( const Vector3& position, PickupType type, double amount_weapon_model, ulong respawnInterval, double ammo ) :
+	Element( nullptr )
+{
+	this->LuaUserdata = Lua::Bindings::CreatePickup( LuaVM, position, type, amount_weapon_model, respawnInterval, ammo );
+}
+
 Pickup::~Pickup()
 {
+}
+
+PickupType Pickup::GetType() const
+{
+	uchar value;
+
+	Lua::Bindings::GetPickupType( LuaVM, LuaUserdata, value );
+
+	return (PickupType)value;
+}
+
+uchar Pickup::GetWeapon() const
+{
+	uchar value;
+
+	Lua::Bindings::GetPickupWeapon( LuaVM, LuaUserdata, value );
+
+	return value;
+}
+
+float Pickup::GetAmount() const
+{
+	float value;
+
+	Lua::Bindings::GetPickupAmount( LuaVM, LuaUserdata, value );
+
+	return value;
+}
+
+ushort Pickup::GetAmmo() const
+{
+	ushort value;
+
+	Lua::Bindings::GetPickupAmmo( LuaVM, LuaUserdata, value );
+
+	return value;
+}
+
+ulong Pickup::GetRespawnInterval() const
+{
+	ulong value;
+
+	Lua::Bindings::GetPickupRespawnInterval( LuaVM, LuaUserdata, value );
+
+	return value;
+}
+
+bool Pickup::IsSpawned() const
+{
+	bool value;
+
+	Lua::Bindings::IsPickupSpawned( LuaVM, LuaUserdata, value );
+
+	return value;
+}
+
+bool Pickup::SetType( PickupType type, double amount_weapon_model, double ammo )
+{
+	return Lua::Bindings::SetPickupType( LuaVM, LuaUserdata, type, amount_weapon_model, ammo );
+}
+
+bool Pickup::SetRespawnInterval( ulong interval )
+{
+	return Lua::Bindings::SetPickupRespawnInterval( LuaVM, LuaUserdata, interval );
+}
+
+bool Pickup::Use( Player* player )
+{
+	return Lua::Bindings::UsePickup( LuaVM, LuaUserdata, player );
 }
--- src/Entity/Pickup.h
+++ src/Entity/Pickup.h
@@ -17,11 +17,31 @@ class Pickup;
 
 #include "Element.h"
 
+enum PickupType
+{
+	Health,
+	Armour,
+	Weapon,
+	Custom,
+};
+
 class Pickup : public Element
 {
 public:
-												Pickup                     ( PVOID luaUserdata );
-	virtual                                     ~Pickup                    ();
+									Pickup               ( PVOID luaUserdata );
+	                                Pickup               ( const Vector3& position, PickupType type, double amount_weapon_model, ulong respawnInterval = 30000, double ammo = 50 );
+	virtual                         ~Pickup              ();
+
+	virtual PickupType              GetType              () const;
+	virtual uchar                   GetWeapon            () const;
+	virtual float                   GetAmount            () const;
+	virtual ushort                  GetAmmo              () const;
+	virtual ulong                   GetRespawnInterval   () const;
+	virtual bool                    IsSpawned            () const;
+
+	virtual bool                    SetType              ( PickupType type, double amount_weapon_model, double ammo );
+	virtual bool                    SetRespawnInterval   ( ulong interval );
+	virtual bool                    Use                  ( Player* player );
 };
 
 #endif
