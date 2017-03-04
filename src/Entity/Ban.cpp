78c92d60f50f653d438a4c7107df99bf1502cff5
Kernell <kernell.it@gmail.com>
2017-03-04 17:02:16 +0300

    Добавлены MTA методы для Ban

--- src/Entity/Ban.cpp
+++ src/Entity/Ban.cpp
@@ -13,11 +13,118 @@
 #include "StdInc.h"
 #include "Ban.h"
 
+#include "Core/Module.h"
+
 Ban::Ban( PVOID luaUserdata ) :
 	Element( luaUserdata )
 {
 }
 
+Ban::Ban( string IP, string username, string serial, Element* responsible, string reason, time_t unban ) :
+	Element( nullptr )
+{
+	this->LuaUserdata = Lua::Bindings::AddBan( LuaVM, IP, username, serial, responsible, reason, unban );
+}
+
 Ban::~Ban()
 {
+}
+
+bool Ban::Remove( Element* responsible )
+{
+	if( Lua::Bindings::RemoveBan( LuaVM, LuaUserdata, responsible ) )
+	{
+		delete this;
+
+		return true;
+	}
+
+	return false;
+}
+
+string Ban::GetIP() const
+{
+	string value;
+
+	Lua::Bindings::GetBanIP( LuaVM, LuaUserdata, value );
+
+	return value;
+}
+
+string Ban::GetSerial() const
+{
+	string value;
+
+	Lua::Bindings::GetBanSerial( LuaVM, LuaUserdata, value );
+
+	return value;
+}
+
+string Ban::GetUsername() const
+{
+	string value;
+
+	Lua::Bindings::GetBanUsername( LuaVM, LuaUserdata, value );
+
+	return value;
+}
+
+string Ban::GetNick() const
+{
+	string value;
+
+	Lua::Bindings::GetBanNick( LuaVM, LuaUserdata, value );
+
+	return value;
+}
+
+string Ban::GetReason() const
+{
+	string value;
+
+	Lua::Bindings::GetBanReason( LuaVM, LuaUserdata, value );
+
+	return value;
+}
+
+string Ban::GetAdmin() const
+{
+	string value;
+
+	Lua::Bindings::GetBanAdmin( LuaVM, LuaUserdata, value );
+
+	return value;
+}
+
+time_t Ban::GetTime() const
+{
+	time_t value;
+
+	Lua::Bindings::GetBanTime( LuaVM, LuaUserdata, value );
+
+	return value;
+}
+
+time_t Ban::GetUnbanTime() const
+{
+	time_t value;
+
+	Lua::Bindings::GetUnbanTime( LuaVM, LuaUserdata, value );
+
+	return value;
+}
+
+bool Ban::SetUnbanTime( time_t time )
+{
+	return Lua::Bindings::SetUnbanTime( LuaVM, LuaUserdata, time );
+}
+
+bool Ban::SetReason( const string& reason )
+{
+	return Lua::Bindings::SetBanReason( LuaVM, LuaUserdata, reason );
+}
+
+bool Ban::SetAdmin( const string& adminName )
+{
+	return Lua::Bindings::SetBanAdmin( LuaVM, LuaUserdata, adminName );
 }
--- src/Entity/Ban.h
+++ src/Entity/Ban.h
@@ -20,8 +20,28 @@ class Ban;
 class Ban : public Element
 {
 public:
-												Ban                     ( PVOID luaUserdata );
-	virtual                                     ~Ban                    ();
+									Ban                     ( PVOID luaUserdata );
+	                                Ban                     ( string IP, string username, string serial, Element* responsible = nullptr, string reason = "", time_t unban = 0 );
+	virtual                         ~Ban                    ();
+
+	virtual bool					Remove                  ( Element* responsible = nullptr );
+
+///	static  vector< Ban* >          GetBans                 ( lua_State* pLuaVM );
+///	static  bool                    ReloadBanList           ( lua_State* pLuaVM );
+
+	virtual string                  GetIP                   () const;
+	virtual string                  GetSerial               () const;
+	virtual string                  GetUsername             () const;
+	virtual string                  GetNick                 () const;
+	virtual string                  GetReason               () const;
+	virtual string                  GetAdmin                () const;
+
+	virtual time_t                  GetTime                 () const;
+	virtual time_t                  GetUnbanTime            () const;
+
+	virtual bool                    SetUnbanTime            ( time_t time );
+	virtual bool                    SetReason               ( const string& reason );
+	virtual bool                    SetAdmin                ( const string& adminName );
 };
 
 #endif
