51aaaa252b55c9bd2e6538802261b2d8c12a84a4
Kernell <kernell.it@gmail.com>
2017-03-18 01:13:53 +0300

    Реализован EntityRepository для работы с данными моделей (Entity) и связки typename => table

---
+++ src/Repository/EntityRepository.h
@@ -0,0 +1,113 @@
+/*********************************************************
+*
+*  Copyright © 2017, Xen Roleplay.
+*
+*  All Rights Reserved.
+*
+*  Redistribution and use in source and binary forms,
+*  with or without modification,
+*  is permitted only for authors.
+*
+*********************************************************/
+
+#pragma once
+
+#include <StdInc.h>
+#include "IEntityRepository.h"
+#include "Core/MySQL.h"
+#include "Core/MySQLResult.h"
+
+class ElementManager;
+class DatabaseManager;
+
+template< typename T >
+class EntityRepository : public IEntityRepository
+{
+public:
+	EntityRepository( DatabaseManager* db ) :
+		DB( db )
+	{
+	}
+
+	virtual ~EntityRepository()
+	{
+		this->Entities.clear();
+	}
+
+	virtual T* GetByID( const string& id )
+	{
+		return this->GetByID( stoi( id.c_str() ) );
+	}
+
+	virtual T* GetByID( uint64_t id )
+	{
+		if( !this->DB->IsConnected() )
+		{
+			return nullptr;
+		}
+
+		return nullptr;
+	}
+
+	virtual vector< T* > SelectAll( const char* order = nullptr, const char* orderBy = nullptr, int limit = -1 )
+	{
+		vector< T* > result;
+
+		if( !this->DB->IsConnected() )
+		{
+			return result;
+		}
+
+		string query = "SELECT ";
+
+		{
+			const TableColumns& tableColumns = this->DB->GetTableSchema( this->GetTableName() );
+
+			bool _first = true;
+
+			for( const auto& col : tableColumns )
+			{
+				if( _first )
+				{
+					query += ", ";
+
+					_first = false;
+				}
+
+				query += "t." + col.Name;
+			}
+		}
+
+		query += " FROM " + this->GetTableName() + " t ORDER BY t." + ( orderBy ? orderBy : "id" ) + " " + ( order ? order : "ASC" );
+
+		MySQLResult* mysql_result = this->DB->Query( query );
+
+		if( mysql_result )
+		{
+			for( MySQLRow row : *mysql_result )
+			{
+				string id = row[ "id" ];
+
+				T* entity = this->Entities[ id ];
+
+				if( !entity )
+				{
+					T* entity = this->CreateFromQuery( row );
+				}
+
+				result.push_back( entity );
+			}
+
+			mysql_result->Free();
+		}
+
+		return result;
+	}
+
+	virtual T* CreateFromQuery( const MySQLRow& row ) = 0;
+
+protected:
+	DatabaseManager*  DB;
+
+	map< string, T* > Entities;
+};
---
+++ src/Repository/IEntityRepository.h
@@ -0,0 +1,22 @@
+/*********************************************************
+*
+*  Copyright © 2017, Xen Roleplay.
+*
+*  All Rights Reserved.
+*
+*  Redistribution and use in source and binary forms,
+*  with or without modification,
+*  is permitted only for authors.
+*
+*********************************************************/
+
+#pragma once
+
+#include <StdInc.h>
+
+class IEntityRepository
+{
+public:
+	virtual string GetName() const = 0;
+	virtual string GetTableName() const = 0;
+};
