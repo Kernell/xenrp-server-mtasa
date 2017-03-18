2083095978c6553bb26656c93271bd7bce24ec62
Kernell <kernell.it@gmail.com>
2017-03-18 20:28:46 +0300

    Реализован класс CRC32

---
+++ src/SharedUtil/CRC32.h
@@ -0,0 +1,26 @@
+/*********************************************************
+*
+*  Copyright © 2017, Xen Roleplay.
+*
+*  All Rights Reserved.
+*
+*  Redistribution and use in source and binary forms,
+*  with or without modification,
+*  is permitted only for authors.
+*
+*********************************************************/
+
+#pragma once
+
+class CRC32;
+
+#ifndef __CRC32_H
+#define __CRC32_H
+
+class CRC32
+{
+public:
+	static uint32_t Calc( const LPBYTE pdata, const uint32_t size, DWORD crc = 0xFFFF );
+};
+
+#endif
